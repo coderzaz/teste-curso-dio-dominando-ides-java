303df7c2e31c3ce189e3402c47f24ce167d584d6
Paulo <coderzaz@gmail.com>
2021-11-04 09:17:30 -0300

    First commit

---
+++ src/primeiro_programa/Main.java
@@ -0,0 +1,20 @@
+package primeiro_programa;
+
+import primeiro_programa.model.Gato;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Gato gato = new Gato("lau","preto",4);
+		
+		System.out.println(gato);
+		
+		
+		/*int a = 2;
+		int b = 3;
+		
+		System.out.println("Ola mundo\n"+(a+b));	*/	
+	}
+
+}
